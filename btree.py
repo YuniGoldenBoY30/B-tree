4bdd0118b672aff17da939d281205e9705832881
Yuniel Silva <ysilva@tm.cupet.cu>
2023-03-30 17:02:58 -0400

    clean code

--- btree.py
+++ btree.py
@@ -11,15 +11,15 @@ class BTreeNode:
 
 # Tree
 class BTree:
-    def __init__(self, number_tree):
+    def __init__(self, min_order_tree):
         self.root = BTreeNode(True)
-        self.number_tree = number_tree
+        self.min_order_tree = min_order_tree
         
         # Insert node
     
     def insert(self, k):
         root = self.root
-        if len(root.keys) == (2 * self.number_tree) - 1:
+        if len(root.keys) == (2 * self.min_order_tree) - 1:
             temp = BTreeNode()
             self.root = temp
             temp.child.insert(0, root)
@@ -30,37 +30,37 @@ class BTree:
         
         # Insert nonfull
     
-    def insert_non_full(self, x, k):
-        i = len(x.keys) - 1
-        if x.leaf:
-            x.keys.append((None, None))
-            while i >= 0 and k[0] < x.keys[i][0]:
-                x.keys[i + 1] = x.keys[i]
+    def insert_non_full(self, nodo, key):
+        i = len(nodo.keys) - 1
+        if nodo.leaf:
+            nodo.keys.append((None, None))
+            while i >= 0 and key[0] < nodo.keys[i][0]:
+                nodo.keys[i + 1] = nodo.keys[i]
                 i -= 1
-            x.keys[i + 1] = k
+            nodo.keys[i + 1] = key
         else:
-            while i >= 0 and k[0] < x.keys[i][0]:
+            while i >= 0 and key[0] < nodo.keys[i][0]:
                 i -= 1
             i += 1
-            if len(x.child[i].keys) == (2 * self.number_tree) - 1:
-                self.split_child(x, i)
-                if k[0] > x.keys[i][0]:
+            if len(nodo.child[i].keys) == (2 * self.min_order_tree) - 1:
+                self.split_child(nodo, i)
+                if key[0] > nodo.keys[i][0]:
                     i += 1
-            self.insert_non_full(x.child[i], k)
+            self.insert_non_full(nodo.child[i], key)
         
         # Split the child
     
-    def split_child(self, x, i):
-        t = self.number_tree
-        y = x.child[i]
+    def split_child(self, nodo, i):
+        min_order_tree = self.min_order_tree
+        y = nodo.child[i]
         z = BTreeNode(y.leaf)
-        x.child.insert(i + 1, z)
-        x.keys.insert(i, y.keys[t - 1])
-        z.keys = y.keys[t: (2 * t) - 1]
-        y.keys = y.keys[0: t - 1]
+        nodo.child.insert(i + 1, z)
+        nodo.keys.insert(i, y.keys[min_order_tree - 1])
+        z.keys = y.keys[min_order_tree: (2 * min_order_tree) - 1]
+        y.keys = y.keys[0: min_order_tree - 1]
         if not y.leaf:
-            z.child = y.child[t: 2 * t]
-            y.child = y.child[0: t - 1]
+            z.child = y.child[min_order_tree: 2 * min_order_tree]
+            y.child = y.child[0: min_order_tree - 1]
     
     # Print the tree
     def print_tree(self, x, l = 0):
@@ -74,19 +74,17 @@ class BTree:
                 self.print_tree(i, l)
     
     # Search key in the tree
-    def search_key(self, k, x = None):
+    def search_key(self, key, x = None):
         if x is not None:
             i = 0
-            while i < len(x.keys) and k > x.keys[i][0]:
+            while i < len(x.keys) and key > x.keys[i][0]:
                 i += 1
-            if i < len(x.keys) and k == x.keys[i][0]:
+            if i < len(x.keys) and key == x.keys[i][0]:
                 return (x, i)
             elif x.leaf:
                 return None
             else:
-                return self.search_key(k, x.child[i])
+                return self.search_key(key, x.child[i])
         
         else:
-            return self.search_key(k, self.root)
-
-
+            return self.search_key(key, self.root)
