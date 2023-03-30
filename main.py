a95d6bc85e226ade4115ec34a21af7d969c67c5b
Yuniel Silva <ysilva@tm.cupet.cu>
2023-03-30 17:01:45 -0400

    testing code

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from btree import BTree
 def main():
     B = BTree(3)
     
-    for i in range(50):
+    for i in range(1, 10):
         B.insert((i, 2 * i))
     
     B.print_tree(B.root)
     
-    if B.search_key(8) is not None:
+    if B.search_key(12) is not None:
         print("\nFound")
     else:
         print("\nNot Found")
